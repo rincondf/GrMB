b3d2e815c088c4cb10eea3d05572f9edb5963089
Rincon <diego.rincon@wsu.edu>
2024-09-05 14:32:09 -0700

    Create simulation model and output in Fig4

---
+++ Figure4.R
@@ -0,0 +1,108 @@
+# Figure 4
+require(MASS)
+
+a = (t1[which.max(rowSums(sta_A4))] - ((coef(alt1A)[1] - 1) / coef(alt1A)[2]))
+b = ((t1[which.max(rowSums(sta_A4A))] - a) - ((coef(alt2A)[1] - 1) / coef(alt2A)[2])) / 2
+
+
+t_col <- function(color, percent = 50, name = NULL) {
+  #      color = color name
+  #    percent = % transparency
+  #       name = an optional name for the color
+  
+  ## Get RGB values for named color
+  rgb.val <- col2rgb(color)
+  
+  ## Make new color using input color as base and alpha set by transparency
+  t.col <- rgb(rgb.val[1], rgb.val[2], rgb.val[3],
+               max = 255,
+               alpha = (100 - percent) * 255 / 100,
+               names = name)
+  
+  ## Save the color
+  invisible(t.col)
+}
+
+
+
+eggcol <- t_col("grey", percent = 50)
+crowcol <- t_col("brown", percent = 50)
+nymcol <- t_col("brown", percent = 80)
+malecol <- t_col("darkgreen", percent = 40)
+femcol <- t_col("blue4", percent = 40)
+
+
+# phenology
+par(mfrow = c(3, 1), oma = c(6, 6, 1, 1))
+
+par(mar = c(2, 2, 2, 2) + 0.1)
+plot(seq(0, long, stepy) - (a + b), rowSums(sta_A) / 1000, type = "l", ylab = "", xlab = "",
+     xlim = c(-310, 1700), yaxt = "n", xaxt = "n", cex.lab = 2, cex.axis = 2, lwd = 2)
+
+axis(2, at = seq(0, 1, 0.2), labels = FALSE)
+axis(3, at = c(-(a + b), 0), labels = c("Eggs laid", "Jan 1st"), cex.axis = 1.5)
+axis(1, at = c(seq(-500, -100, 200), 0, seq(100, 1700, 200)), labels = FALSE)
+
+lines(seq(0, long, stepy) - (a + b), rowSums(sta_A5) / 10000, lwd = 2)
+lines(seq(0, long, stepy) - (a + b), rowSums(sta_A5A) / 300000, lwd = 2)
+polygon(c(seq(0, long, stepy) - (a + b), - (a + b)), c(rowSums(sta_A) / 1000, 0), col = eggcol, border = NA)
+polygon(c(seq(0, long, stepy) - (a + b), - (a + b)), c(rowSums(sta_A5) / 10000, 0), col = eggcol, border = NA)
+polygon(c(seq(0, long, stepy) - (a + b), 1900), c(rowSums(sta_A5A) / 300000, 0), col = eggcol, border = NA)
+mtext("A", cex = 1.7, adj = -0.07)
+
+
+plot(seq(0, long, stepy) - (a + b), rowSums(sta_A1) / 1000, type = "l", ylab = "", xlab = "",
+     xlim = c(-310, 1700), yaxt = "n", xaxt = "n", cex.lab = 2, cex.axis = 2, lwd = 2)
+
+axis(2, at = seq(0, 1, 0.2), labels = FALSE)
+axis(3, at = c(-(a + b), 0), labels = c("Eggs laid", "Jan 1st"), cex.axis = 1.5)
+axis(1, at = c(seq(-500, -100, 200), 0, seq(100, 1700, 200)), labels = FALSE)
+
+lines(seq(0, long, stepy) - (a + b), rowSums(sta_A1A) / 17000, lwd = 2)
+polygon(c(seq(0, long, stepy) - (a + b), - (a + b)), c(rowSums(sta_A1) / 1000, 0), col = crowcol, border = NA)
+polygon(c(seq(0, long, stepy) - (a + b), - (a + b)), c(rowSums(sta_A1A) / 17000, 0), col = crowcol, border = NA)
+
+lines(seq(0, long, stepy) - (a + b), rowSums(sta_A2) / 600, lwd = 2, lty = 3)
+lines(seq(0, long, stepy) - (a + b), rowSums(sta_A3) / 650, lwd = 2, lty = 3)
+polygon(c(seq(0, long, stepy) - (a + b), - (a + b)), c(rowSums(sta_A2) / 600, 0), col = nymcol, border = NA)
+polygon(c(seq(0, long, stepy) - (a + b), - (a + b)), c(rowSums(sta_A3) / 650, 0), col = nymcol, border = NA)
+
+lines(seq(0, long, stepy) - (a + b), rowSums(sta_A2A) / 9000, lwd = 2, lty = 3)
+lines(seq(0, long, stepy) - (a + b), rowSums(sta_A3A) / 12000, lwd = 2, lty = 3)
+polygon(c(seq(0, long, stepy) - (a + b), - (a + b)), c(rowSums(sta_A2A) / 9000, 0), col = nymcol, border = NA)
+polygon(c(seq(0, long, stepy) - (a + b), - (a + b)), c(rowSums(sta_A3A) / 12000, 0), col = nymcol, border = NA)
+
+
+mtext("B", cex = 1.7, adj = -0.07)
+
+
+
+plot(seq(0, long, stepy) - (a + b), rowSums(sta_A4) / 45, type = "l", ylab = "", xlab = "",
+     xlim = c(-310, 1700), yaxt = "n", xaxt = "n", cex.lab = 2, cex.axis = 2, lwd = 2, ylim = c(0, 0.75))
+
+axis(2, at = seq(0, 1, 0.2), labels = FALSE)
+axis(3, at = c(-(a + b), 0), labels = c("Eggs laid", "Jan 1st"), cex.axis = 1.5)
+axis(1, at = c(seq(-500, -100, 200), 0, seq(100, 1700, 200)), cex.axis = 2)
+
+lines(seq(0, long, stepy) - (a + b), rowSums(sta_A4a) / 600, lwd = 2)
+lines(seq(0, long, stepy) - (a + b), rowSums(sta_A4A) / 1100, lwd = 2)
+lines(seq(0, long, stepy) - (a + b), rowSums(sta_A4Aa) / 19000, lwd = 2)
+
+polygon(c(seq(0, long, stepy) - (a + b), - (a + b)), c(rowSums(sta_A4) / 45, 0), col = malecol, border = NA)
+polygon(c(seq(0, long, stepy) - (a + b), - (a + b)), c(rowSums(sta_A4a) / 600, 0), col = femcol, border = NA)
+
+polygon(c(seq(0, long, stepy) - (a + b), - (a + b)), c(rowSums(sta_A4A) / 1100, 0), col = malecol, border = NA)
+polygon(c(seq(0, long, stepy) - (a + b), 1900), c(rowSums(sta_A4Aa) / 19000, 0), col = femcol, border = NA)
+
+points(dataG1A$DD2, dataG1A$prop, col = "green3", lwd = 2)
+points(dataG2A$DD2, dataG2A$prop, col = "green3", lwd = 2)
+
+abline(v = (coef(alt1A)[1] - 1) / coef(alt1A)[2], col = "blue", lwd = 2, lty = 2)
+abline(v = t1[which.max(rowSums(sta_A4))] - (a + b), lwd = 2, lty = 2)
+
+abline(v = (coef(alt2A)[1] - 1) / coef(alt2A)[2], col = "brown", lwd = 2, lty = 2)
+abline(v = t1[which.max(rowSums(sta_A4A))] - (a + b), lwd = 2, lty = 2)
+mtext("C", cex = 1.7, adj = -0.07)
+
+title(ylab = "Relative number", outer = TRUE, cex.lab = 2.5)
+title(xlab = "Degree days", outer = TRUE, cex.lab = 2.5)
---
+++ Simulation_model.R
@@ -0,0 +1,205 @@
+library(MASS)
+
+euler.m <- function(f, h = 1e-7, x0, y0, xfinal) {
+  N = (xfinal - x0) / h
+  x = y = numeric(N + 1)
+  x[1] = x0; y[1] = y0
+  i = 1
+  while (i <= N) {
+    x[i + 1] = x[i] + h
+    y[i + 1] = y[i] + h * f(x[i], y[i])
+    i = i + 1
+  }
+  return(data.frame(X = x, Y = y))
+}
+
+
+
+del_e <- (25 - 10) * 7.6
+del_cr <- (25 - 10) * 14.29
+del2ndA <- (24.4 - 10) * 8.3
+del3rdA <- (24.4 - 10) * 12.2
+del_mA <-(24.4 - 10) * 1.4
+del_fA <- (24.4 - 10) * 34.6
+
+
+preOv <- (25 - 10) * 22.14
+repT <- (25 - 10) * 31.77
+
+eggR <- 83.9 / repT
+
+ke <- round(((25 - 10) * 7.6)^2 / (((25 - 10) * 0.68) * sqrt(31))^2)
+kcr <- round(((25 - 10) * 14.29)^2 / (((25 - 10) * 0.26) * sqrt(392))^2)
+k2ndA <- round(((24.4 - 10) * 8.3)^2 / (((24.4 - 10) * 1.3) * sqrt(4))^2)
+k3rdA <- round(((24.4 - 10) * 12.2)^2 / (((24.4 - 10) * 2.6) * sqrt(11))^2)
+k_mA <- ceiling(((24.4 - 10) * 1.4)^2 / (((24.4 - 10) * 0.8) * sqrt(7))^2)
+k_fA <- round(((24.4 - 10) * 34.6)^2 / (((24.4 - 10) * 8.4) * sqrt(10))^2)
+
+k_f_rep <- round(((25 - 10) * (54.84 - 22.14))^2 / (((25 - 10) * 1.227) * sqrt(64))^2)
+
+
+stepy <- 0.0625
+long <- 2500
+t1 <- seq(0, long, stepy)
+
+
+# FIRST GENERATION
+
+eggs = 1000 
+craw = 0
+n2 = 0
+n3 = 0
+males = 0
+females = 0
+eggsII = 0
+
+sta_A <- matrix(0, length(t1), ke); sta_A[1, 1] <- eggs
+check <- rep(c(0, 1), long)
+
+for(h in 1: length(t1)) {
+  for(t in 1: (length(t1) - 1)) {
+    for(i in 1: (ke - 1)) {
+      sta_A[t + 1, 1] <- euler.m(function(x, y){ -((y) * (ke / del_e))}, h = stepy,
+                                 x0 = t1[t], y0 = sta_A[t, 1], xfinal = t1[t + 1])$Y[2]
+      sta_A[t + 1, i + 1] <- euler.m(function(x, y){(sta_A[t, i] * (ke / del_e)) - ((y) * (ke / del_e))},
+                                     h = stepy, x0 = t1[t], y0 = sta_A[t, i + 1], xfinal = t1[t + 1])$Y[2]
+    }
+  }
+  
+  ######
+  sta_A1 <- matrix(0, length(t1), kcr)
+  sta_A1[1, 1] <- craw
+  for(t in 1: (length(t1) - 1)) {
+    for(i in 1: (kcr - 1)) {
+      sta_A1[t + 1, 1] <- euler.m(function(x, y){(sta_A[t, ke] * (ke / del_e)) - ((y) * (kcr / del_cr))}, 
+                                  h = stepy, x0 = t1[t], y0 = sta_A1[t, 1], xfinal = t1[t + 1])$Y[2]
+      sta_A1[t + 1, i + 1] <- euler.m(function(x, y){(sta_A1[t, i] * (kcr / del_cr)) - ((y) * (kcr / del_cr))}, 
+                                      h = stepy, x0 = t1[t],  y0 = sta_A1[t, i + 1], xfinal = t1[t + 1])$Y[2]
+    }
+  }
+  #####
+  
+  sta_A2 <- matrix(0, length(t1), k2ndA)
+  sta_A2[1, 1] <- n2
+  for(t in 1: (length(t1) - 1)) {
+    for(i in 1: (k2ndA - 1)) {
+      sta_A2[t + 1, 1] <- euler.m(function(x, y){(sta_A1[t, kcr] * (kcr / del_cr)) - ((y) * (k2ndA / del2ndA))}, 
+                                  h = stepy, x0 = t1[t], y0 = sta_A2[t, 1], xfinal = t1[t + 1])$Y[2]
+      sta_A2[t + 1, i + 1] <- euler.m(function(x, y){(sta_A2[t, i] * (k2ndA / del2ndA)) - ((y) * (k2ndA / del2ndA))}, 
+                                      h = stepy, x0 = t1[t],  y0 = sta_A2[t, i + 1], xfinal = t1[t + 1])$Y[2]
+    }
+  }
+  #####
+  
+  sta_A3 <- matrix(0, length(t1), k3rdA)
+  sta_A3[1, 1] <- n3
+  for(t in 1: (length(t1) - 1)) {
+    for(i in 1: (k3rdA - 1)) {
+      sta_A3[t + 1, 1] <- euler.m(function(x, y){(sta_A2[t, k2ndA] * (k2ndA / del2ndA)) - ((y) * (k3rdA / del3rdA))}, 
+                                  h = stepy, x0 = t1[t], y0 = sta_A3[t, 1], xfinal = t1[t + 1])$Y[2]
+      sta_A3[t + 1, i + 1] <- euler.m(function(x, y){(sta_A3[t, i] * (k3rdA / del3rdA)) - ((y) * (k3rdA / del3rdA))}, 
+                                      h = stepy, x0 = t1[t],  y0 = sta_A3[t, i + 1], xfinal = t1[t + 1])$Y[2]
+    }
+  }
+  #####
+  
+  sta_A4 <- matrix(0, length(t1), k_mA)
+  sta_A4[1, 1] <- males
+  for(t in 1: (length(t1) - 1)) {
+    sta_A4[t + 1, 1] <- euler.m(function(x, y){(sta_A3[t, k3rdA] * (0.5) * (k3rdA / del3rdA)) - ((y) * (k_mA / del_mA))}, 
+                                h = stepy, x0 = t1[t], y0 = sta_A4[t, 1], xfinal = t1[t + 1])$Y[2]
+  }
+  #####
+  
+  sta_A4a <- matrix(0, length(t1), k_fA)
+  sta_A4a[1, 1] <- females
+  for(t in 1: (length(t1) - 1)) {
+    for(i in 1: (k_fA - 1)) {
+      sta_A4a[t + 1, 1] <- euler.m(function(x, y){(sta_A3[t, k3rdA] * (0.5) * (k3rdA / del3rdA)) - ((y) * (k_fA / del_fA))}, 
+                                   h = stepy, x0 = t1[t], y0 = sta_A4a[t, 1], xfinal = t1[t + 1])$Y[2]
+      sta_A4a[t + 1, i + 1] <- euler.m(function(x, y){(sta_A4a[t, i] * (k_fA / del_fA)) - ((y) * (k_fA / del_fA))}, 
+                                       h = stepy, x0 = t1[t],  y0 = sta_A4a[t, i + 1], xfinal = t1[t + 1])$Y[2]
+    }
+  }
+  #####
+  
+  sta_A5 <- matrix(0, length(t1), ke)
+  sta_A5[1, 1] <- eggsII
+  for(t in 1: (length(t1) - 1)) {
+    for(i in 1: (ke - 1)) {
+      sta_A5[t + 1, 1] <- euler.m(function(x, y){(sum(sta_A4a[t, ]) * eggR) - ((y) * (ke / del_e))}, 
+                                  h = stepy, x0 = t1[t], y0 = sta_A5[t, 1], xfinal = t1[t + 1])$Y[2]
+      sta_A5[t + 1, i + 1] <- euler.m(function(x, y){(sta_A5[t, i] * (ke / del_e)) - ((y) * (ke / del_e))}, 
+                                      h = stepy, x0 = t1[t],  y0 = sta_A5[t, i + 1], xfinal = t1[t + 1])$Y[2]
+    }
+  }
+  
+  ####
+  
+  sta_A1A <- matrix(0, length(t1), kcr)
+  for(t in 1: (length(t1) - 1)) {
+    for(i in 1: (kcr - 1)) {
+      sta_A1A[t + 1, 1] <- euler.m(function(x, y){(sta_A5[t, ke] * (ke / del_e)) - ((y) * (kcr / del_cr))}, 
+                                   h = stepy, x0 = t1[t], y0 = sta_A1A[t, 1], xfinal = t1[t + 1])$Y[2]
+      sta_A1A[t + 1, i + 1] <- euler.m(function(x, y){(sta_A1A[t, i] * (kcr / del_cr)) - ((y) * (kcr / del_cr))}, 
+                                       h = stepy, x0 = t1[t],  y0 = sta_A1A[t, i + 1], xfinal = t1[t + 1])$Y[2]
+    }
+  }
+  
+  #####
+  
+  sta_A2A <- matrix(0, length(t1), k2ndA)
+  for(t in 1: (length(t1) - 1)) {
+    for(i in 1: (k2ndA - 1)) {
+      sta_A2A[t + 1, 1] <- euler.m(function(x, y){(sta_A1A[t, kcr] * (kcr / del_cr)) - ((y) * (k2ndA / del2ndA))}, 
+                                   h = stepy, x0 = t1[t], y0 = sta_A2A[t, 1], xfinal = t1[t + 1])$Y[2]
+      sta_A2A[t + 1, i + 1] <- euler.m(function(x, y){(sta_A2A[t, i] * (k2ndA / del2ndA)) - ((y) * (k2ndA / del2ndA))}, 
+                                       h = stepy, x0 = t1[t],  y0 = sta_A2A[t, i + 1], xfinal = t1[t + 1])$Y[2]
+    }
+  }
+  #####
+  
+  sta_A3A <- matrix(0, length(t1), k3rdA)
+  for(t in 1: (length(t1) - 1)) {
+    for(i in 1: (k3rdA - 1)) {
+      sta_A3A[t + 1, 1] <- euler.m(function(x, y){(sta_A2A[t, k2ndA] * (k2ndA / del2ndA)) - ((y) * (k3rdA / del3rdA))}, 
+                                   h = stepy, x0 = t1[t], y0 = sta_A3A[t, 1], xfinal = t1[t + 1])$Y[2]
+      sta_A3A[t + 1, i + 1] <- euler.m(function(x, y){(sta_A3A[t, i] * (k3rdA / del3rdA)) - ((y) * (k3rdA / del3rdA))}, 
+                                       h = stepy, x0 = t1[t],  y0 = sta_A3A[t, i + 1], xfinal = t1[t + 1])$Y[2]
+    }
+  }
+  #####
+  
+  sta_A4A <- matrix(0, length(t1), k_mA)
+  for(t in 1: (length(t1) - 1)) {
+    sta_A4A[t + 1, 1] <- euler.m(function(x, y){(sta_A3A[t, k3rdA] * (0.5) * (k3rdA / del3rdA)) - ((y) * (k_mA / del_mA))}, 
+                                 h = stepy, x0 = t1[t], y0 = sta_A4A[t, 1], xfinal = t1[t + 1])$Y[2]
+  }
+  #####
+  
+  sta_A4Aa <- matrix(0, length(t1), k_fA)
+  for(t in 1: (length(t1) - 1)) {
+    for(i in 1: (k_fA - 1)) {
+      sta_A4Aa[t + 1, 1] <- euler.m(function(x, y){(sta_A3A[t, k3rdA] * (0.5) * (k3rdA / del3rdA)) - ((y) * (k_fA / del_fA))}, 
+                                    h = stepy, x0 = t1[t], y0 = sta_A4Aa[t, 1], xfinal = t1[t + 1])$Y[2]
+      sta_A4Aa[t + 1, i + 1] <- euler.m(function(x, y){(sta_A4Aa[t, i] * (k_fA / del_fA)) - ((y) * (k_fA / del_fA))}, 
+                                        h = stepy, x0 = t1[t],  y0 = sta_A4Aa[t, i + 1], xfinal = t1[t + 1])$Y[2]
+    }
+  }
+  #####
+  
+  sta_A5A <- matrix(0, length(t1), ke)
+  for(t in 1: (length(t1) - 1)) {
+    for(i in 1: (ke - 1)) {
+      sta_A5A[t + 1, 1] <- euler.m(function(x, y){(sum(sta_A4Aa[t, ]) * eggR) - ((y) * (ke / del_e))}, 
+                                   h = stepy, x0 = t1[t], y0 = sta_A5A[t, 1], xfinal = t1[t + 1])$Y[2]
+      sta_A5A[t + 1, i + 1] <- euler.m(function(x, y){(sta_A5A[t, i] * (ke / del_e)) - ((y) * (ke / del_e))}, 
+                                       h = stepy, x0 = t1[t],  y0 = sta_A5A[t, i + 1], xfinal = t1[t + 1])$Y[2]
+    }
+  }
+  
+  ####
+  
+  check[h + 1] <- sta_A[length(t1),1]
+  if(check[h + 1] == check[h]) break
+}
