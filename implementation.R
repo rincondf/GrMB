65569cd3025dcd145214017bc6230147bc37d2a0
Rincon <diego.rincon@wsu.edu>
2025-06-11 17:57:35 -0700

    implementation updated

--- implementation.R
+++ implementation.R
@@ -1,24 +1,11 @@
+load("simulation_output_F.RData")
+load("gammamodelsF.RData")
 
-
-plot(seq(0, long, stepy) - (a + b), rowSums(sta_A1) / 1000, type = "l", ylab = "", xlab = "",
-     xlim = c(-310, 1700), yaxt = "n", xaxt = "n", cex.lab = 2, cex.axis = 2, lwd = 2)
-
-axis(2, at = seq(0, 1, 0.2), labels = FALSE)
-axis(3, at = c(-(a + b), 0), labels = c("Eggs laid", "Jan 1st"), cex.axis = 1.8)
-axis(1, at = c(seq(-500, -100, 200), 0, seq(100, 1700, 200)), cex.axis = 2.2)
-
-
-
-craw1_raw <- rowSums(sta_A1)
-
+crwD <- data.frame(x = seq(0, longF, stepy) - (a + b), y = rowSums(sta_A1F))
 
 library(SuppDists)
 library(ExtDist)
 library(bbmle)
-
-
-eJohnsonSB(X = crwD$x, w = crwD$y*100)
-
 
 
 estimat <- function(DDs, prs, method){
@@ -46,7 +33,6 @@ estimat <- function(DDs, prs, method){
 
 
 MLL2 <- estimat(DDs = crwD$x, prs = crwD$y, method = "Nelder-Mead")
-
 summary(MLL2)
 
 
@@ -60,6 +46,7 @@ funres2 <- function(DDs){
              lambda = lambda)
 }
 
+
 funres2a <- function(DDs){
   xi = coef(MLL2)[3]
   lambda = coef(MLL2)[4] - coef(MLL2)[3]
@@ -69,123 +56,247 @@ funres2a <- function(DDs){
              lambda = lambda)
 }
 
-plot(DDs, prop_cr1*15, type = "l", xlim = c(-330, 500))
-lines(DDs, funres2(DDs))
-lines(DDs[1:10000], dJohnsonSB(
-  DDs[1:10000],
-  gamma = mod1$par[1],
-  delta = mod1$par[2],
-  xi = mod1$par[3],
-  lambda = mod1$par[4]), col = "red")
 
-plot(DDs, cumsum(prop_cr1), type = "l", xlim = c(-330, 500))
-lines(DDs, funres2a(DDs))
+plot(crwD$x, crwD$y/364081.8, type = "l", xlim = c(-558, 3060))
+lines(crwD$x, funres2(crwD$x), col = "red")
 
+plot(crwD$x, cumsum(crwD$y/sum(crwD$y)), type = "l", xlim = c(-558, 3060))
+lines(crwD$x, funres2a(crwD$x), col = "red")
 
+crawlers1st <- data.frame(DDs = unique(round(crwD$x)),
+                          simModelRel = (crwD$y[c(grep("4966", as.character(crwD$x)), 
+                                             grep("0033", as.character(crwD$x)))] / 364081.8) * 10000,
+                          JohnsonSBRel = funres2(unique(round(crwD$x))) * 10000,
+                          simModelCum = cumsum((crwD$y / sum(crwD$y[c(grep("4966", as.character(crwD$x)), 
+                                                                      grep("0033", as.character(crwD$x)))]))[c(grep("4966", as.character(crwD$x)), 
+                                                        grep("0033", as.character(crwD$x)))]),
+                          JohnsonSBCum = funres2a(unique(round(crwD$x))))
 
+plot(crawlers1st$DDs, crawlers1st$simModelRel, type = "l", xlim = c(-558, 3060))
+lines(crawlers1st$DDs, crawlers1st$JohnsonSBRel, col = "red")
 
-DDs <- seq(0, long, stepy) - (a + b)
+plot(crawlers1st$DDs, crawlers1st$simModelCum, type = "l", xlim = c(-558, 3060))
+lines(crawlers1st$DDs, crawlers1st$JohnsonSBCum, col = "red")
 
-
-plot(DDs, craw1_raw / sum(craw1_raw))
-
-prop_cr1 <- craw1_raw / sum(craw1_raw)
-
-dJohnsonSB(
-  DDs,
-  gamma = -0.5,
-  delta = 2,
-  xi = -0.5,
-  lambda = 2)
+###########
 
 
+n2D <- data.frame(x = seq(0, longF, stepy) - (a + b), y = rowSums(sta_A2F))
 
-crwD <- data.frame(x = DDs[1:10000], y = prop_cr1[1:10000])
+MLL2n2 <- estimat(DDs = n2D$x, prs = n2D$y, method = "Nelder-Mead")
+summary(MLL2n2)
 
 
-minf <- function(data, pars1) {
-  
-  abs(sum((pJohnsonSB(data$x, 
-                      gamma = pars1[1], 
-                      delta = pars1[2], 
-                      xi = pars1[3], 
-                      lambda = pars1[4]) - (cumsum(data$y)))))
-  
-  
+
+funres2n2 <- function(DDs){
+  xi = coef(MLL2n2)[3]
+  lambda = coef(MLL2n2)[4] - coef(MLL2n2)[3]
+  dJohnsonSB(DDs, gamma = coef(MLL2n2)[1],
+             delta = coef(MLL2n2)[2],
+             xi = xi,
+             lambda = lambda)
 }
 
 
+funres2an2 <- function(DDs){
+  xi = coef(MLL2n2)[3]
+  lambda = coef(MLL2n2)[4] - coef(MLL2n2)[3]
+  pJohnsonSB(DDs, gamma = coef(MLL2n2)[1],
+             delta = coef(MLL2n2)[2],
+             xi = xi,
+             lambda = lambda)
+}
 
-mod1 <- optim(par = c(gamma = 0.756, delta = 1.30, xi = -330.3, lambda = 628), data = crwD, minf, method = "Nelder-Mead")
+plot(n2D$x, n2D$y/213902, type = "l", xlim = c(-558, 3060))
+lines(n2D$x, funres2n2(n2D$x), col = "red")
 
-
-plot(DDs, cumsum(prop_cr1), type = "l")
-lines(DDs, pJohnsonSB(
-  DDs,
-  gamma = mod1$par[1],
-  delta = mod1$par[2],
-  xi = mod1$par[3],
-  lambda = mod1$par[4]), col = "red")
-
-
-plot(DDs[1:10000], prop_cr1[1:10000] * 15, type = "l")
-lines(DDs[1:10000], dJohnsonSB(
-  DDs[1:10000],
-  gamma = mod1$par[1],
-  delta = mod1$par[2],
-  xi = mod1$par[3],
-  lambda = mod1$par[4]), col = "red")
-
-
-
-plot(DDs, prop_cr1*10, type = "l")
-lines(DDs, dJohnsonSB(
-  DDs,
-  gamma = 1.07,
-  delta = 1.23,
-  xi = -310,
-  lambda = 577), col = "red")
-
-
-
-plot(DDs, dJohnsonSB(
-  DDs,
-  gamma = 1.07,
-  delta = 1.23,
-  xi = 69,
-  lambda = 577), col = "red")
-
-
-
-dJohnsonSB(
-  578,
-  gamma = 1.07,
-  delta = 1.23,
-  xi = 69,
-  lambda = 577)
+plot(n2D$x, cumsum(n2D$y/sum(n2D$y)), type = "l", xlim = c(-558, 3060))
+lines(n2D$x, funres2an2(n2D$x), col = "red")
 
 
 
 
 
+nym2nd <- data.frame(DDs = unique(round(n2D$x)),
+                     simModelRel = (n2D$y[c(grep("4966", as.character(n2D$x)), 
+                                             grep("0033", as.character(n2D$x)))] / 213902) * 10000,
+                     JohnsonSBRel = funres2n2(unique(round(n2D$x))) * 10000,
+                     simModelCum = cumsum((n2D$y / sum(n2D$y[c(grep("4966", as.character(n2D$x)), 
+                                                                 grep("0033", as.character(n2D$x)))]))[c(grep("4966", as.character(n2D$x)), 
+                                                                                                          grep("0033", as.character(n2D$x)))]),
+                     JohnsonSBCum = funres2an2(unique(round(n2D$x))))
 
-t <- seq(-4, 2, 0.01)
-res <- rep(NA, length(t))
+plot(nym2nd$DDs, nym2nd$simModelRel, type = "l", xlim = c(-558, 3060))
+lines(nym2nd$DDs, nym2nd$JohnsonSBRel, col = "red")
+
+plot(nym2nd$DDs, nym2nd$simModelCum, type = "l", xlim = c(-558, 3060))
+lines(nym2nd$DDs, nym2nd$JohnsonSBCum, col = "red")
 
 
-for(i in 1:length(t)) {
-  res[i] <- abs(sum(dJohnsonSB(
-    DDs,
-    gamma = t[i],
-    delta = 1.23,
-    xi = -310,
-    lambda = 600) - prop_cr1))
+
+###########
+
+
+n3D <- data.frame(x = seq(0, longF, stepy) - (a + b), y = rowSums(sta_A3F))
+
+MLL2n3 <- estimat(DDs = n3D$x, prs = n3D$y, method = "Nelder-Mead")
+summary(MLL2n3)
+
+
+
+funres2n3 <- function(DDs){
+  xi = coef(MLL2n3)[3]
+  lambda = coef(MLL2n3)[4] - coef(MLL2n3)[3]
+  dJohnsonSB(DDs, gamma = coef(MLL2n3)[1],
+             delta = coef(MLL2n3)[2],
+             xi = xi,
+             lambda = lambda)
 }
 
 
-plot(t, res)
+funres2an3 <- function(DDs){
+  xi = coef(MLL2n3)[3]
+  lambda = coef(MLL2n3)[4] - coef(MLL2n3)[3]
+  pJohnsonSB(DDs, gamma = coef(MLL2n3)[1],
+             delta = coef(MLL2n3)[2],
+             xi = xi,
+             lambda = lambda)
+}
+
+
+max(n3D$y)/max(funres2n3(n3D$x))
+
+plot(n3D$x, n3D$y/332798.9, type = "l", xlim = c(-558, 3060))
+lines(n3D$x, funres2n3(n2D$x), col = "red")
+
+plot(n3D$x, cumsum(n3D$y/sum(n3D$y)), type = "l", xlim = c(-558, 3060))
+lines(n3D$x, funres2an3(n3D$x), col = "red")
+
+
+nym3rd <- data.frame(DDs = unique(round(n3D$x)),
+                     simModelRel = (n3D$y[c(grep("4966", as.character(n3D$x)), 
+                                            grep("0033", as.character(n3D$x)))] / 332798.9) * 10000,
+                     JohnsonSBRel = funres2n3(unique(round(n3D$x))) * 10000,
+                     simModelCum = cumsum((n3D$y / sum(n3D$y[c(grep("4966", as.character(n3D$x)), 
+                                                               grep("0033", as.character(n3D$x)))]))[c(grep("4966", as.character(n3D$x)), 
+                                                                                                       grep("0033", as.character(n3D$x)))]),
+                     JohnsonSBCum = funres2an3(unique(round(n3D$x))))
+
+plot(nym3rd$DDs, nym3rd$simModelRel, type = "l", xlim = c(-558, 3060))
+lines(nym3rd$DDs, nym3rd$JohnsonSBRel, col = "red")
+
+plot(nym3rd$DDs, nym3rd$simModelCum, type = "l", xlim = c(-558, 3060))
+lines(nym3rd$DDs, nym3rd$JohnsonSBCum, col = "red")
 
 
 
 
+###########
 
+
+malD <- data.frame(x = seq(0, longF, stepy) - (a + b), y = rowSums(sta_A4F))
+
+MLL2ma <- estimat(DDs = malD$x, prs = malD$y, method = "Nelder-Mead")
+summary(MLL2ma)
+
+
+
+funres2ma <- function(DDs){
+  xi = coef(MLL2ma)[3]
+  lambda = coef(MLL2ma)[4] - coef(MLL2ma)[3]
+  dJohnsonSB(DDs, gamma = coef(MLL2ma)[1],
+             delta = coef(MLL2ma)[2],
+             xi = xi,
+             lambda = lambda)
+}
+
+
+funres2ama <- function(DDs){
+  xi = coef(MLL2ma)[3]
+  lambda = coef(MLL2ma)[4] - coef(MLL2ma)[3]
+  pJohnsonSB(DDs, gamma = coef(MLL2ma)[1],
+             delta = coef(MLL2ma)[2],
+             xi = xi,
+             lambda = lambda)
+}
+
+
+max(malD$y)/max(funres2ma(malD$x))
+
+plot(malD$x, malD$y/19058.79, type = "l", xlim = c(-558, 3060))
+lines(malD$x, funres2ma(malD$x), col = "red")
+
+plot(malD$x, cumsum(malD$y/sum(malD$y)), type = "l", xlim = c(-558, 3060))
+lines(malD$x, funres2ama(malD$x), col = "red")
+
+
+males <- data.frame(DDs = unique(round(malD$x)),
+                     simModelRel = (malD$y[c(grep("4966", as.character(malD$x)), 
+                                            grep("0033", as.character(malD$x)))] / 19058.79) * 10000,
+                     JohnsonSBRel = funres2ma(unique(round(malD$x))) * 10000,
+                     simModelCum = cumsum((malD$y / sum(malD$y[c(grep("4966", as.character(malD$x)), 
+                                                               grep("0033", as.character(malD$x)))]))[c(grep("4966", as.character(malD$x)), 
+                                                                                                       grep("0033", as.character(malD$x)))]),
+                     JohnsonSBCum = funres2ama(unique(round(malD$x))))
+
+plot(males$DDs, males$simModelRel, type = "l", xlim = c(-558, 3060))
+lines(males$DDs, males$JohnsonSBRel, col = "red")
+
+plot(males$DDs, males$simModelCum, type = "l", xlim = c(-558, 3060))
+lines(males$DDs, males$JohnsonSBCum, col = "red")
+
+
+
+###########
+
+
+femD <- data.frame(x = seq(0, longF, stepy) - (a + b), y = rowSums(sta_A4aF))
+
+MLL2fem <- estimat(DDs = femD$x, prs = femD$y, method = "Nelder-Mead")
+summary(MLL2fem)
+
+
+
+funres2fem <- function(DDs){
+  xi = coef(MLL2fem)[3]
+  lambda = coef(MLL2fem)[4] - coef(MLL2fem)[3]
+  dJohnsonSB(DDs, gamma = coef(MLL2fem)[1],
+             delta = coef(MLL2fem)[2],
+             xi = xi,
+             lambda = lambda)
+}
+
+
+funres2afem <- function(DDs){
+  xi = coef(MLL2fem)[3]
+  lambda = coef(MLL2fem)[4] - coef(MLL2fem)[3]
+  pJohnsonSB(DDs, gamma = coef(MLL2fem)[1],
+             delta = coef(MLL2fem)[2],
+             xi = xi,
+             lambda = lambda)
+}
+
+
+max(femD$y)/max(funres2fem(femD$x))
+
+plot(femD$x, femD$y/576427.1, type = "l", xlim = c(-558, 3060))
+lines(femD$x, funres2fem(femD$x), col = "red")
+
+plot(femD$x, cumsum(femD$y/sum(femD$y)), type = "l", xlim = c(-558, 3060))
+lines(femD$x, funres2afem(femD$x), col = "red")
+
+
+females <- data.frame(DDs = unique(round(femD$x)),
+                    simModelRel = (femD$y[c(grep("4966", as.character(femD$x)), 
+                                            grep("0033", as.character(femD$x)))] / 576427.1) * 10000,
+                    JohnsonSBRel = funres2fem(unique(round(femD$x))) * 10000,
+                    simModelCum = cumsum((femD$y / sum(femD$y[c(grep("4966", as.character(femD$x)), 
+                                                                grep("0033", as.character(femD$x)))]))[c(grep("4966", as.character(femD$x)), 
+                                                                                                         grep("0033", as.character(femD$x)))]),
+                    JohnsonSBCum = funres2afem(unique(round(femD$x))))
+
+plot(females$DDs, females$simModelRel, type = "l", xlim = c(-558, 3060))
+lines(females$DDs, females$JohnsonSBRel, col = "red")
+
+plot(females$DDs, females$simModelCum, type = "l", xlim = c(-558, 3060))
+lines(females$DDs, females$JohnsonSBCum, col = "red")
